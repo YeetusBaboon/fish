f4b3831cf9b8ef37b575937ad2bc562e55534a6e
YeetusBaboon <yeetus.baboon@gmail.com>
2022-04-27 21:54:03 -0500

    accidentally made color values hex codes

--- conf.d/mountain.fish
+++ conf.d/mountain.fish
@@ -1,20 +1,20 @@
 # Mountain color palette
 
 # --> special
-set -l foreground #f0f0f0 
-set -l selection #4c4c4c 
+set -l foreground f0f0f0 
+set -l selection 4c4c4c 
 
 # --> palette
-set -l teal #9ec3c4
-set -l flamingo #c49ec4
-set -l magenta #a39ec4
-set -l pink #ac8aac
-set -l red #ac8a8c
-set -l peach #c4c19e
-set -l green #8aac8b
-set -l yellow #aca98a 
-set -l blue #8aabac
-set -l gray #e7e7e7
+set -l teal 9ec3c4
+set -l flamingo c49ec4
+set -l magenta a39ec4
+set -l pink ac8aac
+set -l red ac8a8c
+set -l peach c4c19e
+set -l green 8aac8b
+set -l yellow aca98a 
+set -l blue 8aabac
+set -l gray e7e7e7
 
 # Syntax Highlighting
 set -g fish_color_normal $foreground
